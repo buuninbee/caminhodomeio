2910ead0c0bf91fdc9d98a998dc3104c0d0a5927
buuninbee <buuninbee@gmail.com>
2025-01-30 20:50:36 -0300

    feat: making badge responsive

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,7 +1,7 @@
 const Badge = ({number, path}) => {
   return (
-    <div className="flex gap-3 py-2 rounded-lg bg-onSurface text-lg font-serif justify-center">
-        <h3 className="text-segundary">{number}- </h3>
+    <div className="flex gap-3 py-2 rounded-lg bg-onSurface text-lg font-serif justify-center sm:py-3 sm:text-xl md:py-4">
+        <h3 className="text-segundary">{number} - </h3>
         <p className="text-primary ">{path}</p>
     </div>
   )
